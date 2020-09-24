974b4b386fe82a8c90300fe597584becd49bb7c7
tekshila <67528077+tekshila@users.noreply.github.com>
2020-09-23 23:06:28 -0400

    redirect to enrolled course main page.

--- taxila/src/main/java/taxila/controllers/users/MyCoursesController.java
+++ taxila/src/main/java/taxila/controllers/users/MyCoursesController.java
@@ -7,11 +7,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import taxila.controllers.stripe.ChargeRequest;
 import taxila.dao.CourseRepository;
 import taxila.dao.EnrolCourseRepository;
+import taxila.dao.UserRepository;
 import taxila.domain.Course;
 import taxila.domain.EnrolledCourse;
+import taxila.domain.TekUser;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,12 +33,15 @@ public class MyCoursesController {
     @Autowired
     EnrolCourseRepository enrolCourseRepository;
 
+    UserRepository userRepository;
+
     @RequestMapping(value = "/{userId}/mycourses")
-    public String viewMyCourses(@PathVariable String userId, Model model) {
+    public String viewMyCourses(@PathVariable int userId, Model model) {
         logger.info(" userId path variable -------> " + userId);
-        List<EnrolledCourse> enrolledCourses = enrolCourseRepository.findEnrolledCourseByUserId(userId);
+        List<EnrolledCourse> enrolledCourses = enrolCourseRepository.findEnrolledCourseByUserIdentity("" + userId);
         List<Course> mycourses = this.findCourses(enrolledCourses);
             model.addAttribute("mycourses",mycourses);
+            model.addAttribute("userId",userId);
         return "mycourses";
     }
 
@@ -45,4 +53,16 @@ public class MyCoursesController {
         return courses;
     }
 
+
+    @RequestMapping(value = "/view/{courseId}", method = {RequestMethod.GET})
+    public String viewCourse(@PathVariable(name = "courseId") String courseId, Model model, HttpSession httpSession) {
+//        EnrolledCourse course = enrolCourseRepository.findEnrolledCourseByUserId()
+        return "courses/preparing_for_system_design/design_mint";
+    }
+
+    @RequestMapping(value = "/view/{courseId}/{userId}", method = {RequestMethod.GET})
+    public String viewCourse(@PathVariable(name = "courseId") String courseId,@PathVariable(name = "userId") String userId, Model model, HttpSession httpSession) {
+//        EnrolledCourse course = enrolCourseRepository.findEnrolledCourseByUserId()
+        return "courses/preparing_for_system_design/design_mint";
+    }
 }
