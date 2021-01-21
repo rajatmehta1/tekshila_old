10c730c4dc021491531e29465658c2050344af0b
tekshila <67528077+tekshila@users.noreply.github.com>
2021-01-21 11:11:26 -0500

    Updated
    Updated for changes.

--- taxila/src/main/java/taxila/App.java
+++ taxila/src/main/java/taxila/App.java
@@ -11,8 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App 
 {
 
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
     }
 
